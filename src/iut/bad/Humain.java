4bb2661f5285f67fdd4e33070f10d572963ec5e1
Safiharti <safidyharitianantenaina@gmail.com>
2024-03-29 15:33:46 +0300

    version 1.2.0 : creation des méthodes detail et toString dans le classe Humain

--- src/iut/bad/Humain.java
+++ src/iut/bad/Humain.java
@@ -3,19 +3,26 @@ package iut.bad;
 public class Humain {
 	private String nom;
 	private String prenom;
-	private int age;
-	
+	private int age;	
 		
 	public Humain() {
 		
 	}
-
 
 	public Humain(String nom, String prenom, int age) {
 		super();
 		this.nom = nom;
 		this.prenom = prenom;
 		this.age = age;
+	}
+	
+	public void detail() {
+		System.out.println(toString());
+	}
+
+	@Override
+	public String toString() {
+		return "Humain [nom=" + nom + ", prenom=" + prenom + ", age=" + age + "]";
 	}
 	
 	
