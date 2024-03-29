acb834c9a8cccc44bd8a8662f1a2c61b1e89f7f8
Safiharti <safidyharitianantenaina@gmail.com>
2024-03-29 16:27:55 +0300

    version 1.4.1 : ajout de methode main dans la classe femme pour declarer femme ami de homme

--- src/iut/bad/Femme.java
+++ src/iut/bad/Femme.java
@@ -10,4 +10,11 @@ public class Femme extends Humain{
 	public Femme(String nom, String prenom, int age) {
 		super(nom, prenom, age);
 	}
+	
+	public static void main(String[] args) {
+    	Humain femme = new Humain("Marie", "Jeanne", 30);
+    	Humain homme = new Humain("Robert", "Pilier", 35);
+
+    	femme.ami(homme);
+    }
 }
